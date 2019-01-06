00f01a27130d46a1c94ab82be83dc362d8bbf21e
tengerye <yetengqi@gmail.com>
2019-01-06 16:47:46 +0800

    cross-validation for knn

--- defense.py
+++ defense.py
@@ -9,6 +9,7 @@ import argparse, sys, os
 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import pairwise_distances
+from sklearn.model_selection import cross_val_score
 import time
 import torch
 import torch.nn as nn
@@ -71,7 +72,14 @@ class TripletInfer(object): # TODO
         # TODO: check training_set shape and type
         self.train_x = self.model.get_embedding(torch.from_numpy(x))
 
-        self.knn = KNeighborsClassifier(n_neighbors=3) # TODO: cross-validation
+        # cross validation to determine the best k
+        self.k_candidates = [1, 3, 5, 7, 9]
+        scores = []
+        for k in self.k_candidates:
+            self.knn = KNeighborsClassifier(n_neighbors=k)
+            scores.append(cross_val_score(self.knn, self.train_x.detach().numpy(), y, cv=4).mean())
+
+        self.k = self.k_candidates[np.argmax(scores)] # k of knn
         self.knn.fit(self.train_x.detach().numpy(), y)
 
     def __call__(self, x):
@@ -141,7 +149,101 @@ def imshow(img):
 
 
 
-def test(args):
+def normal_attack(args):
+    '''
+    TODO: fabricate an adversarial attack.
+    1. [x] Triplet loss is different from softmax loss. Perform correct loss.
+    2. [x] Perform correct gradient and generate corresponding adversarial examples with labels.
+    3. [ ] Exceptional case: (adversarial examples can be ordered).
+    4. [ ] Show test result and sample some successful and failed adversarial examples.
+    '''
+    device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
+
+    # Prepare dataset.
+    train_dataset = MNIST(args.data_path, train=True, download=True,
+                          transform=transforms.Compose([
+                              transforms.ToTensor(),
+                              transforms.Normalize((params.mean,), (params.std,))
+                          ]))
+    test_dataset = MNIST(args.data_path, train=False, download=True,
+                         transform=transforms.Compose([
+                             transforms.ToTensor(),
+                             transforms.Normalize((params.mean,), (params.std,))
+                         ]))
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=params.batch_size,
+                                               shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1,
+                                              shuffle=False, **kwargs)
+
+    # kNN training
+    train_images, train_labels = loader2numpy(train_loader)
+    model = torch.load('/home/tenger/research/metric-learning/siamese-triplet/triplet.pt')
+    ti = TripletInfer(model, train_images, train_labels)
+
+    # Accuracy counter
+    correct = 0
+    adv_examples = []
+
+    # Loop over all examples in test set
+    for data, target in test_loader:
+
+        # Send the data and label to the device
+        data, target = data.to(device), target.to(device)
+
+        # Set requires_grad attribute of tensor. Important for Attack
+        data.requires_grad = True
+
+        # Forward pass the data through the model
+        output, _ = ti(data)
+
+        # If the initial prediction is wrong, dont bother attacking, just move on
+        if output[0].item() != target.item():
+            continue
+
+        # Calculate the loss
+        loss = F.nll_loss(torch.from_numpy(np.expand_dims(output, axis=0)), target)
+
+        # Zero all existing gradients
+        model.zero_grad()
+
+        # Calculate gradients of model in backward pass
+        loss.backward()
+
+        # Collect datagrad
+        data_grad = data.grad.data
+
+        # Call FGSM Attack
+        perturbed_data = fgsm_attack(data, params.epsilon, data_grad)
+
+        # Re-classify the perturbed image
+        output = ti(perturbed_data)
+
+        # Check for success
+        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        if final_pred.item() == target.item():
+            correct += 1
+            # Special case for saving 0 epsilon examples
+            if (params.epsilon == 0) and (len(adv_examples) < 5):
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+        else:
+            # Save some adv examples for visualization later
+            if len(adv_examples) < 5:
+                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+
+    # Calculate final accuracy for this epsilon
+    final_acc = correct/float(len(test_loader))
+    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(params.epsilon, correct, len(test_loader), final_acc))
+
+    # Return the accuracy and an adversarial example
+    return final_acc, adv_examples
+
+
+
+def ti_attack(args):
     # Prepare dataset.
     train_dataset = MNIST(args.data_path, train=True, download=True,
                           transform=transforms.Compose([
@@ -179,17 +281,16 @@ def test(args):
                                  )
 
     hard_examples = []
-    k=3
     loss_fn = TripletLoss(params.margin)
 
     cnt = 0
-    for row_idx in range(n_train, n_train+n_test):
+    for row_idx in range(n_train, n_train+n_test): # Run only on test set.
         row = dis_mat[row_idx, :n_train]
 
-        # nearest neighbors and corresponding confidences
-        nn_indices = row.argsort()[:k]
+        # Nearest neighbors and corresponding confidences
+        nn_indices = row.argsort()[:ti.k]
         nn_labels = train_labels[nn_indices]
-        confidence = max(np.bincount(nn_labels))/k
+        confidence = max(np.bincount(nn_labels))/ti.k
 
         # Hard, positive examples.
         if np.argmax(np.bincount(nn_labels)) == test_labels[row_idx-n_train] \
@@ -253,7 +354,8 @@ def test(args):
 
 
 def main(args):
-    test(args)
+    ti_attack(args)
+    # normal_attack(args)
 
 if __name__ == "__main__":
     main(parse_arguments(sys.argv[1:]))
